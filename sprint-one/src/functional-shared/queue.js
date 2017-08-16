c47be8a6a5ba3fc5f93577983c8adad4b1f891bf
Hack Reactor Student <student@hackreactor.com>
2017-08-16 17:05:49 -0500

    completed func and func share

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,43 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  var newQueue = {
+    counter: 0
+  };
+
+  _.extend(newQueue, queueMethods);
+
+  return newQueue;
+
 };
 
 var queueMethods = {};
 
+queueMethods.size = function() {
+  if (this.counter >= 0) {
+    return this.counter;
+  } else {
+    return this.counter = 0;
+  }
+};
+
+queueMethods.enqueue = function(val) {
+  var count = this.size();
+  this[count] = val;
+  this.counter++;
+};
+
+queueMethods.dequeue = function() {
+  let popped = this[0];
+  delete this[0];
+  this.counter--;
+  
+  for (var key in this) {
+    this[parseInt(key) - 1] = this[parseInt(key)]; 
+    delete this[parseInt(key)];
+  }
+
+  return popped;
+};
 

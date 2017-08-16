d3d8b219588c473a7beb702457787a8ae6e5b1ce
Hack Reactor Student <student@hackreactor.com>
2017-08-16 12:31:31 -0500

    functional stack completed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,32 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.size()] = value;
+    counter++;
   };
 
   someInstance.dequeue = function() {
+    let popped = storage[0]; 
+    delete storage[0];
+    counter--;
+    //Adjust keys based on new index.
+    for (var key in storage) {
+      key = parseInt(key) - 1;
+    }
+    return popped;
   };
 
   someInstance.size = function() {
+    if (counter >= 0) {
+      return counter;
+    } else {
+      return counter = 0;
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,30 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var counter = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    //console.log('before push: ', storage, value);
+    storage[someInstance.size()] = value;
+    counter++;
+    //console.log('after push: ', storage, value);
   };
-
+  
   someInstance.pop = function() {
+    let popped = storage[someInstance.size() - 1]; 
+    console.log('Before pop: ', popped, storage);
+    delete storage[someInstance.size() - 1];
+    counter--;
+    console.log('After pop: ', popped, storage);
+    return popped;
   };
 
   someInstance.size = function() {
+    if (counter >= 0) {
+      return counter;
+    } else {
+      return counter = 0;
+    }  
   };
 
   return someInstance;

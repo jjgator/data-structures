737bb62de373130910b031a17831f5f2dc854d61
Hack Reactor Student <student@hackreactor.com>
2017-08-16 19:38:27 -0500

    we crushed it

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.counter = 1;
 };
 
+Queue.prototype.size = function() {
+  if (this.counter <= 1) {
+    return 0;
+  } else {
+    return this.counter - 1;
+  }
+};
 
+Queue.prototype.enqueue = function(val) {
+  var count = this.size();
+  this[count] = val;
+  this.counter++;
+};
+
+Queue.prototype.dequeue = function() {
+  var count = this.size();
+  let popped = this[0];
+  delete this[0];
+  for (var key in this) {
+    if (key !== 'counter') {
+      this[parseInt(key) - 1] = this[parseInt(key)];
+      delete this[parseInt(key)];
+    }
+  }
+  this.counter--;
+  return popped;
+};
+
+//first key index to be 0, but equals 1
+//transform key to number, decrement by 1
+//set transformed key equal to original value
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.counter = 0;
+
 };
 
+Stack.prototype.size = function() {
+  if (this.counter >= 0) {
+    return this.counter;
+  } else {
+    return this.counter = 0;
+  }
+};
 
+Stack.prototype.push = function(val) {
+  var count = this.size();
+  this[count] = val;
+  this.counter++;
+};
+
+Stack.prototype.pop = function() {
+  var count = this.size();
+  let popped = this[count - 1];
+  delete this[count - 1];
+  this.counter--;
+  return popped;
+};
